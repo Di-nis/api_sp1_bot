6322afbf7522029612e488121399903172ee6712
Denis Smirnov <di-nis@ya.ru>
2020-08-18 00:12:29 +0300

    v.2 18.08.2020

--- homework.py
+++ homework.py
@@ -12,38 +12,45 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-api_praktikum = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+API_PRAKTIKUM = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+proxy = telegram.utils.request.Request(proxy_url='HTTPS://18.130.89.239:80')
+bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
 logger = logging.getLogger('my_logger')
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
+    homework_status = homework.get('status')
+    homework_name = homework.get('homework_name')
+    if homework_status is None or homework_name is None:
+        raise ValueError('Неверный ответ сервера')
+        logger.error('Неверный ответ сервера')
+    elif homework_status == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
-def get_homework_statuses(current_timestamp):
+def get_homework_statuses(current_timestamp=None):
+    if current_timestamp is None:
+        current_timestamp = int(time.time())
     headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     params = {'from_date': current_timestamp}
     try:
-        homework_statuses = requests.get('{}'.format(api_praktikum),
+        homework_statuses = requests.get('{}'.format(API_PRAKTIKUM),
                                          headers=headers,
                                          params=params)
-        return homework_statuses.json()
-    except Exception as err:
-        logger.error('Ошибка соедиения с Практикум')
+    except Exception as e:
+        logger.error(f'Ошибка соедиения с Практикум {e}')
+    return homework_statuses.json()
 
 
 def send_message(text):
     try:
-        proxy = telegram.utils.request.Request(proxy_url='socks5://45.91.93.166:1080')
-        bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
-        return bot.send_message(chat_id=CHAT_ID, text=text)
-    except Exception as err:
-        logger.error('Ошибка соедиения с Telegram')
+        bot.send_message(chat_id=CHAT_ID, text=text)
+    except Exception as e:
+        logger.error(f'Ошибка соедиения с Telegram: {e}')
+    return bot.send_message(chat_id=CHAT_ID, text=text)
 
 
 def main():
